afc2cdd06524cdc81b438755340c8b2e74011cfc
Sarosh <saroshabdullah2013@gmail.com>
2021-07-18 17:01:36 +0530

    Added mediaquery for button width

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -19,6 +20,11 @@ const Button = styled.button`
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
+
 `;
 
 // const Button = props => {
